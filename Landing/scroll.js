04aefb275842059809d77192599481b08df57ffe
Diego Bermudez <diego.bermudez@wayni.pe>
2024-08-12 15:50:28 -0500

    problems fixed

---
+++ Landing/scroll.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    scrollNav();
+});
+
+function scrollNav() {
+    // Selecciona todos los enlaces que apunten a un ID
+    const navLinks = document.querySelectorAll('a[href^="#"]');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+}
